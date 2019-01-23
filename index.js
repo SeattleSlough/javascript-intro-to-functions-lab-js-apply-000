f4444e9091a169681a45f29ed166f36d9b00f693
Michael Maggs <michaelmaggs@gmail.com>
2019-01-23 18:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma") {
     return "I love you, too"
     
-    elseif (string.toLowerCase)
+    elseif (string.toLowerCase()) {
+      return "I can't hear you"
+    }
+    else (string.toUpperCase())  {
+      return "YES INDEED"
+    }
   }
 
 }
