07aecb1e2c2859aac21250af1dcf05f0d215be1a
Michael Maggs <michaelmaggs@gmail.com>
2019-01-23 18:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function shout(string) {
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma") {
-    log.console("I love you, too")  
+    return "I love you, too"  
   }
 
 }
